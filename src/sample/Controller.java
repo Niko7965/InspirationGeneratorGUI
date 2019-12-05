65cc5ec8d74e42a0f37ca1f35a0804b286135ffd
Niko7965 <54095558+Niko7965@users.noreply.github.com>
2019-12-05 10:58:48 +0100

    #9

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,24 +3,43 @@ package sample;
 import javafx.fxml.FXML;
 
 
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 
 public class Controller {
     public ImageView imgButton;
+    public ImageView backgroundImg;
     public TextGen textgen = new TextGen();
     @FXML private Text TitleText;
     //@FXML private ImageView imgButton;
 
 
 
+    @FXML private void hoverAnimation(MouseEvent event) throws FileNotFoundException {
+        FileInputStream input = new FileInputStream("Images/ButtonHover.png");
+        Image image = new Image(input);
+        imgButton.setImage(image);
+
+    }
+
+    @FXML private void exitAnimation(MouseEvent event) throws FileNotFoundException {
+        FileInputStream input = new FileInputStream("Images/Button.png");
+        Image image = new Image(input);
+        imgButton.setImage(image);
+
+    }
+
     @FXML private void submit(MouseEvent event){
             TitleText.setText(textgen.getInspiration());
-
 
 
     }
