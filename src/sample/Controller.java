9ed94fb1c02733ce8304e72eafba1c8ccd828968
chri81e6 <54137140+chri81e6@users.noreply.github.com>
2019-12-15 13:42:55 +0100

    #9 #5

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -42,8 +42,8 @@ public class Controller {
 
     @FXML private void submit(MouseEvent event) throws FileNotFoundException {
             TitleText.setText(textgen.getInspiration());
-            int rand = ThreadLocalRandom.current().nextInt(1,8);
-            FileInputStream input = new FileInputStream("Images/Backgrounds/"+rand+".png");
+            int rand = ThreadLocalRandom.current().nextInt(1,15);
+            FileInputStream input = new FileInputStream("Images/Backgrounds/a"+rand+".png");
             Image image = new Image(input);
             wallpaper.setImage(image);
     }
